6f9074a22963d4d2719114c44fa2db0c08e1b1ec
Ayhampt <ayhampt011@gmail.com>
2025-11-05 13:11:20 +0530

    setup express server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config({
   path: "./.env",
 });
 
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+  console.log(`Space backend is running on http://localhost:${port}`);
+});
